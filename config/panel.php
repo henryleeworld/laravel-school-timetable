2473fd29d695644cea79376a1bb25df451361b5d
Henry Lee <henryleeworld@gmail.com>
2020-02-20 06:56:16 +0800

    feat:初始化

---
+++ config/panel.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'date_format'         => 'Y-m-d',
+    'time_format'         => 'H:i:s',
+    'lesson_time_format'  => 'H:i',
+    'primary_language'    => 'zh-TW',
+    'available_languages' => [
+        'en' => 'English',
+        'zh-TW' => 'Traditional Chinese'
+    ],
+];
